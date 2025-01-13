3fa2fbb95ae1b04a7c91c77fbc4fbcaf1f835bf4
Jan Hamouz <jan.hamouz@gmail.com>
2025-01-14 00:32:50 +0100

    Update install.sh
    uprava postupu

--- install.sh
+++ install.sh
@@ -13,15 +13,6 @@ apt list --upgradable -y
 echo "Installing necessary packages..."
 sudo apt install -y python3 python3-pip portaudio19-dev python3-fastapi python3-uvicorn lsof git git-lfs docker.io
 
-# Přesun konfiguračního souboru ~/.asoundrc
-echo "Setting up .asoundrc..."
-if [ -f /opt/asoundrc ]; then
-    mv /opt/asoundrc ~/.asoundrc
-    echo "Moved /opt/asoundrc to ~/.asoundrc"
-else
-    echo "Error: /opt/asoundrc not found. Skipping."
-fi
-
 # Instalace Seeed voicecard
 echo "Installing Seeed voicecard..."
 if [ -d /opt/audio/seeed-voicecard ]; then
@@ -29,6 +20,15 @@ if [ -d /opt/audio/seeed-voicecard ]; then
     sudo ./install.sh
 else
     echo "Error: /opt/audio/seeed-voicecard not found. Skipping voicecard installation."
+fi
+
+# Přesun konfiguračního souboru ~/.asoundrc
+echo "Setting up .asoundrc..."
+if [ -f /opt/oldbot_audio/asoundrc ]; then
+    mv /opt/oldbot_audio/asoundrc ~/.asoundrc
+    echo "Moved /opt/oldbot_audio/asoundrc to ~/.asoundrc"
+else
+    echo "Error: /opt/oldbot_audio/asoundrc not found. Skipping."
 fi
 
 # Restart systému
